aa906407963a62f0f344bec896b37479ea447718
Boaz <webevolution13@gmail.com>
2024-05-26 21:59:16 +0200

    upDate
    front end

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 <template>
   <section>
     <div class="services-content">
-      <h2 class="services-content__h2 text-transform-uppercase">
+      <h2 class="services-content__h2 text-transform-uppercase t-end">
         SZOLGÁLTATÁSAINK
       </h2>
       <div class="grid-4">
@@ -36,7 +36,7 @@
         </div>
 
         <div
-          class="services-content__img-text-box services-content__img-text-box--one-bg"
+          class="services-content__img-text-box services-content__img-text-box--two-bg"
         >
           <NuxtImg
             src="/img/services/csapagyak.svg"
@@ -45,11 +45,12 @@
             height="100%"
           />
           <h3 class="services-content__img-text-box__h3 text-center">
-            CSAPÁGYAK
+            GOLYÓSORSÓK
           </h3>
           <p class="services-content__img-text-box__p">
-            Fedezze fel prémium csapágyainkat: precíziós orsócsapágyak és
-            különleges kerámiás csapágyak, minden igényt kielégítve!
+            Japán és dél-koreai kapcsolatainknak köszönhetően biztosítjuk a
+            világszerte ritkán elérhető golyós orsókat, minőségi garanciával és
+            teljes körű gyártási szolgáltatásokkal!
           </p>
           <div class="services-content__img-text-box__link-box t-end">
             <NuxtLink class="services-content__img-text-box__link-box__link">
@@ -64,7 +65,7 @@
         </div>
 
         <div
-          class="services-content__img-text-box services-content__img-text-box--one-bg"
+          class="services-content__img-text-box services-content__img-text-box--three-bg"
         >
           <NuxtImg
             src="/img/services/csapagyak.svg"
@@ -73,11 +74,12 @@
             height="100%"
           />
           <h3 class="services-content__img-text-box__h3 text-center">
-            CSAPÁGYAK
+            LINEÁRTECHNIKA
           </h3>
           <p class="services-content__img-text-box__p">
-            Fedezze fel prémium csapágyainkat: precíziós orsócsapágyak és
-            különleges kerámiás csapágyak, minden igényt kielégítve!
+            Gyorsítsa fel projekteit a szerszámgépiparban nélkülözhetetlen,
+            japán gyártmányú lineáris vezetékekkel és kocsikkal, amelyeket német
+            raktárunkból 1-3 napon belül szállítunk!
           </p>
           <div class="services-content__img-text-box__link-box t-end">
             <NuxtLink class="services-content__img-text-box__link-box__link">
@@ -92,7 +94,7 @@
         </div>
 
         <div
-          class="services-content__img-text-box services-content__img-text-box--one-bg"
+          class="services-content__img-text-box services-content__img-text-box--four-bg"
         >
           <NuxtImg
             src="/img/services/csapagyak.svg"
@@ -101,11 +103,12 @@
             height="100%"
           />
           <h3 class="services-content__img-text-box__h3 text-center">
-            CSAPÁGYAK
+            EGYEDI GYÁRTÁS
           </h3>
           <p class="services-content__img-text-box__p">
-            Fedezze fel prémium csapágyainkat: precíziós orsócsapágyak és
-            különleges kerámiás csapágyak, minden igényt kielégítve!
+            Felejtse el a hosszú hónapokig tartó várakozást alkatrészekre!
+            Üzemünkben kis és nagy sorozatban is gyártunk egyedi alkatrészeket,
+            beleértve a golyós orsók végmegmunkálását is.
           </p>
           <div class="services-content__img-text-box__link-box t-end">
             <NuxtLink class="services-content__img-text-box__link-box__link">
@@ -118,6 +121,47 @@
             </NuxtLink>
           </div>
         </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="about-content d-flex">
+      <div class="about-content__text-box position-relative z-100 bg-color-w">
+        <h2 class="about-content__text-box__h2">Rólunk</h2>
+        <p class="about-content__text-box__p">
+          Cégünk 2014-ben alakult azzal a céllal, hogy egy helyen kínáljunk
+          minden szükséges alkatrészt és segítséget a szerszámgépek,
+          esztergagépek és marógépek javításához és felújításához. Széles
+          termékválasztékunkban megtalálhatóak csapágyak, golyós orsók,
+          precíziós csapágyanyák, kenéstechnikai elemek, valamint lineáris
+          vezetékek és kocsik.
+        </p>
+        <p class="about-content__text-box__p">
+          Elkötelezettek vagyunk amellett, hogy ügyfeleink számára minden
+          szükséges támogatást megadjunk. Amennyiben kérdése vagy kérése merül
+          fel, ne habozzon kapcsolatba lépni velünk telefonon vagy e-mailben.
+          Nálunk a minőség és az ügyfél elégedettség a legfontosabb. Várjuk
+          megkeresését, legyen szó ajánlatkérésről vagy rendelésről – a
+          Precision Bearing Kft.-nél megtalálja, amire szüksége van!
+        </p>
+        <div class="about-content__text-box__link-box">
+          <NuxtLink class="page-nuxt-link text-transform-uppercase">
+            TOVÁBB
+            <NuxtImg
+              src="/img/about/about-arrow-r.svg"
+              alt="Precision Bearing kft"
+              class="page-nuxt-link__img"
+              height="100%"
+            />
+          </NuxtLink>
+        </div>
+      </div>
+      <div class="about-content__img-box">
+        <NuxtImg
+          src="/img/about/about.webp"
+          alt="Precision Bearing kft"
+          class="about-content__img-box__img"
+        />
       </div>
     </div>
   </section>
