5ae3ad49dcabcabbfd47066accb7d4c1d9a601ca
Boaz <webevolution13@gmail.com>
2024-06-05 19:44:15 +0200

    upDate
    front end

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,41 @@
-<script setup></script>
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+interface AccordionItem {
+  title: string
+  content: string
+}
+
+const AccordionItems: AccordionItem[] = [
+  { title: 'Kell-e regisztrálnom a vásárláshoz?', content: 'Tartalom 1' },
+  {
+    title: 'Okostelefonon és tableten is tudok vásárolni?',
+    content:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+  { title: 'Hogy adhatom le a rendelésem?', content: 'Tartalom 3' },
+  {
+    title: 'Lehetséges e-mailben, vagy telefonon is rendelni?',
+    content: 'Tartalom 4',
+  },
+]
+
+const currentIndex = ref<number | null>(null)
+
+const headerClass = (index: number) => {
+  return {
+    'accordion-content__header': true,
+    'red-background': currentIndex.value === index,
+  }
+}
+</script>
 
 <template>
   <section>
     <div class="services-content">
-      <h2 class="services-content__h2 text-transform-uppercase t-end text-color">
+      <h2
+        class="services-content__h2 text-transform-uppercase t-end text-color"
+      >
         SZOLGÁLTATÁSAINK
       </h2>
       <div class="grid-4">
@@ -23,7 +55,9 @@
             Fedezze fel prémium csapágyainkat: precíziós orsócsapágyak és
             különleges kerámiás csapágyak, minden igényt kielégítve!
           </p>
-          <div class="services-content__img-text-box__link-box position-absolute">
+          <div
+            class="services-content__img-text-box__link-box position-absolute"
+          >
             <NuxtLink class="services-content__img-text-box__link-box__link">
               <NuxtImg
                 src="/img/services/r-arrow.svg"
@@ -52,7 +86,9 @@
             világszerte ritkán elérhető golyós orsókat, minőségi garanciával és
             teljes körű gyártási szolgáltatásokkal!
           </p>
-          <div class="services-content__img-text-box__link-box position-absolute">
+          <div
+            class="services-content__img-text-box__link-box position-absolute"
+          >
             <NuxtLink class="services-content__img-text-box__link-box__link">
               <NuxtImg
                 src="/img/services/r-arrow.svg"
@@ -81,7 +117,9 @@
             japán gyártmányú lineáris vezetékekkel és kocsikkal, amelyeket német
             raktárunkból 1-3 napon belül szállítunk!
           </p>
-          <div class="services-content__img-text-box__link-box position-absolute">
+          <div
+            class="services-content__img-text-box__link-box position-absolute"
+          >
             <NuxtLink class="services-content__img-text-box__link-box__link">
               <NuxtImg
                 src="/img/services/r-arrow.svg"
@@ -110,7 +148,9 @@
             Üzemünkben kis és nagy sorozatban is gyártunk egyedi alkatrészeket,
             beleértve a golyós orsók végmegmunkálását is.
           </p>
-          <div class="services-content__img-text-box__link-box position-absolute">
+          <div
+            class="services-content__img-text-box__link-box position-absolute"
+          >
             <NuxtLink class="services-content__img-text-box__link-box__link">
               <NuxtImg
                 src="/img/services/r-arrow.svg"
@@ -137,7 +177,9 @@
         </div>
       </div>
       <div class="about-content__text-box position-absolute z-100 bg-color-w">
-        <h2 class="about-content__text-box__h2 text-transform-uppercase text-color">
+        <h2
+          class="about-content__text-box__h2 text-transform-uppercase text-color"
+        >
           Rólunk
         </h2>
         <p class="about-content__text-box__p text-color">
@@ -157,7 +199,9 @@
           Precision Bearing Kft.-nél megtalálja, amire szüksége van!
         </p>
         <div class="about-content__text-box__link-box">
-          <NuxtLink class="page-nuxt-link text-transform-uppercase text-color-w f-700">
+          <NuxtLink
+            class="page-nuxt-link text-transform-uppercase text-color-w f-700"
+          >
             TOVÁBB
             <NuxtImg
               src="/img/about/about-arrow-r.svg"
@@ -172,7 +216,38 @@
   </section>
   <section>
     <div class="blog-content">
-      <h4 class="blog-content__h4 text-transform-uppercase t-end text-color">blog</h4>
+      <h4 class="blog-content__h4 text-transform-uppercase t-end text-color">
+        blog
+      </h4>
+      <div class="accordion-content position-relative">
+        <div
+          v-for="(item, index) in AccordionItems"
+          :key="index"
+          class="accordion-content__accordion"
+          @click="currentIndex = currentIndex === index ? null : index"
+        >
+          <div class="accordion-content__accordion__flex-box d-flex">
+            <div :class="headerClass(index)">
+              <h3 class="accordion-content__header__h3 f-600">
+                {{ item.title }}
+              </h3>
+              <span
+                class="accordion-content__header__arrow"
+                :class="{ 'accordion-is-open': currentIndex === index }"
+              >
+              </span>
+            </div>
+            <div
+              class="accordion-content__content position-absolute"
+              :class="{ AccordionOpen: currentIndex === index }"
+            >
+              <p class="accordion-content__content__p text-color">
+                {{ item.content }}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
   <section>
@@ -181,7 +256,6 @@
         ÁLTALUNK FORGALMAZOTT MÁRKÁK
       </h5>
       <div class="brands-content__img-content grid-8">
-
         <div class="brands-content__img-content__img-box">
           <NuxtImg
             src="/img/references/fag.svg"
@@ -245,7 +319,6 @@
             class="brands-content__img-content__img-box__img"
           />
         </div>
-
       </div>
     </div>
   </section>
